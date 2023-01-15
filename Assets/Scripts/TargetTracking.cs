26f7f17943e1302cc878041d4c7b3660feaa58b0
Nhan Tran <nhantrantnt@gmail.com>
2023-01-15 10:45:11 -0500

    add two patients waypoints

--- Assets/Scripts/TargetTracking.cs
+++ Assets/Scripts/TargetTracking.cs
@@ -6,16 +6,35 @@ using UnityEngine.AI;
 public class TargetTracking : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public GameObject target;
+    LineRenderer lineRenderer;
     // Start is called before the first frame update
     void Start()
     {
         agent = transform.GetComponent<NavMeshAgent>();
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
     }
 
-    public void SetDestination()
+    public void SetDestination(GameObject target)
     {
         var endPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
         agent.destination = endPoint;
+        DrawPath();
+    }
+
+    public void DrawPath()
+    {
+        var path = new NavMeshPath();
+        NavMesh.CalculatePath(agent.transform.position, agent.destination, NavMesh.AllAreas, path);
+        var positions = path.corners;
+
+        // Draw route
+        lineRenderer.widthMultiplier = 0.1F;
+        lineRenderer.positionCount = positions.Length;
+        for (int i = 0; i < positions.Length; i++)
+        {
+            Debug.Log(" point " + i + " = " + positions[i]);
+            lineRenderer.SetPosition(i, positions[i]);
+        }
     }
 }
